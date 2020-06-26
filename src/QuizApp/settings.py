06d6477877ad84c99f3eb98515d07e1927a14b8e
Suryakant Kumar <suryakant.kumar@mountblue.tech>
2020-06-26 13:35:22 +0530

    add secret key from environment variable

--- src/QuizApp/settings.py
+++ src/QuizApp/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7&)lh)8zr3=#hkz1-fov-(*inhtvc$-_w8p9a@_a)%txe0uiok'
+# SECRET_KEY = '7&)lh)8zr3=#hkz1-fov-(*inhtvc$-_w8p9a@_a)%txe0uiok'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
