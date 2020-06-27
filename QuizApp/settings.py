5bc90ecbca82bfc8387c65684a0709c5422c8fa8
Suryakant Kumar <suryakant.kumar@mountblue.tech>
2020-06-28 01:23:48 +0530

    Disable right click, ctrl, u, f12 keys to protect source code

--- QuizApp/settings.py
+++ QuizApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['brainyquiz.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'brainyquiz.herokuapp.com']
 
 # Application definition
 
