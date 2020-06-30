46ecdc2460d72663c4f894f5bccc6be7b31d4e8b
Suryakant Kumar <suryakant.kumar@mountblue.tech>
2020-06-30 11:57:23 +0530

    fix bug of not adding the same question or quiz title again

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Quiz, Question, Option, QuestionWiseQuizScore
+from .models import Quiz, Question, Option
 
 
 class CreateQuizModelForm(forms.ModelForm):
@@ -12,7 +12,7 @@ class CreateQuizModelForm(forms.ModelForm):
         title = self.cleaned_data.get('title')
         qs = Quiz.objects.filter(title__iexact=title)
         if qs.exists():
-            raise forms.ValidationError("This title is already exists. Please Choose a different one.")
+            raise forms.ValidationError("This Quiz title already exists. Please Choose a different one.")
         return title
 
 
@@ -20,6 +20,13 @@ class CreateQuestionModelForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['title']
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        qs = Question.objects.filter(title__iexact=title)
+        if qs.exists():
+            raise forms.ValidationError("This Question title already exists. Please Choose a different one.")
+        return title
 
 
 class CreateOptionModelForm(forms.ModelForm):
